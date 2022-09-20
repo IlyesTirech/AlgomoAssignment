6910f795e398bb929d40581a523719a5a6878a41
Ilyes Tirech <ilyestirech@Ilyess-MacBook-Pro.local>
2022-09-20 20:39:52 +0100

    Added css

--- frontend/src/components/SearchBar.js
+++ frontend/src/components/SearchBar.js
@@ -1,13 +1,13 @@
 import React, {useEffect, useState} from 'react'
+import styled from 'styled-components';
 import axios from 'axios'
 
 const SearchBar = () => {
 
-
     const [character, setCharacter] = useState();
     const [startsWith, setStartsWith] = useState('');
   useEffect(() => {
-       axios.get(`http://gateway.marvel.com/v1/public/characters?nameStartsWith=${startsWith}&apikey=${process.env.REACT_APP_API_KEY}&hash=${process.env.REACT_APP_HASH_KEY}&limit=100`)
+       axios.get(`http://gateway.marvel.com/v1/public/characters?nameStartsWith=${startsWith}&apikey=${process.env.REACT_APP_API_KEY}&hash=${process.env.REACT_APP_HASH_KEY}&limit=10`)
        .then(res => {
         setCharacter(res.data.data.results)
         console.log(character);
@@ -25,8 +25,10 @@ const SearchBar = () => {
 
   return (
     <>
+    <StyledSearch>
     <input type='text' placeholder='Search Character...' onChange={handleNameChange}/>
-    <button>Submit</button>
+    </StyledSearch>
+    <StyledResults>
     {
       startsWith.length === 0 || character?.length === 0 ? (
         <p>No Characters Found</p>
@@ -37,8 +39,23 @@ const SearchBar = () => {
           )
         })
     }
+    </StyledResults>
     </>
   )
 }
 
 export default SearchBar
+
+const StyledSearch = styled.div`
+  display: flex;
+  justify-content: center;
+  input {
+    padding: 5px;
+  }
+`
+
+const StyledResults = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+`
