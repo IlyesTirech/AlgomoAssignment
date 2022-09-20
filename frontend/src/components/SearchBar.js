34afdd9141fadf06b02bf214e69e9469ae21c983
Ilyes Tirech <ilyestirech@Ilyess-MacBook-Pro.local>
2022-09-20 21:47:55 +0100

    CSS

--- frontend/src/components/SearchBar.js
+++ frontend/src/components/SearchBar.js
@@ -10,7 +10,6 @@ const SearchBar = () => {
        axios.get(`http://gateway.marvel.com/v1/public/characters?nameStartsWith=${startsWith}&apikey=${process.env.REACT_APP_API_KEY}&hash=${process.env.REACT_APP_HASH_KEY}&limit=10`)
        .then(res => {
         setCharacter(res.data.data.results)
-        console.log(character);
        })
        .catch(err => {
         console.log(err);
@@ -28,18 +27,19 @@ const SearchBar = () => {
     <StyledSearch>
     <input type='text' placeholder='Search Character...' onChange={handleNameChange}/>
     </StyledSearch>
-    <StyledResults>
+
     {
       startsWith.length === 0 || character?.length === 0 ? (
-        <p>No Characters Found</p>
+        <StyledErrorHandle>No Characters Found</StyledErrorHandle>
       ) :     
         character && character?.map(c => {
           return (
+            <StyledResults>
             <p>{c.name}</p>
+            </StyledResults>
           )
         })
     }
-    </StyledResults>
     </>
   )
 }
@@ -54,8 +54,27 @@ const StyledSearch = styled.div`
   }
 `
 
+const StyledErrorHandle = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-top: 5px;
+`
+
 const StyledResults = styled.div`
   display: flex;
   align-items: center;
+  justify-content: center;
   flex-direction: column;
+  background-color: white;
+  box-shadow: rgba(0,0,0,0.35) 0px 5px 15px;
+  height: 200px;
+  width: 50vw;
+  margin-top: 10px;
+  margin: auto;
+  overflow: hidden;
+  overflow-y: auto;
+p:hover {
+  background-color: lightgrey;
+  cursor: pointer;
+}
 `
