46f4e116e33d6ad5ef68dfdff500dbd5c132c1a5
Ilyes Tirech <ilyestirech@Ilyess-MacBook-Pro.local>
2022-09-21 00:33:36 +0100

    Added CSS

---
+++ frontend/src/GlobalStyles.js
@@ -0,0 +1,12 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyles = createGlobalStyle`
+    *{
+        text-decoration: none;
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+`
+
+export default GlobalStyles
--- frontend/src/components/SearchBar.js
+++ frontend/src/components/SearchBar.js
@@ -1,80 +1,74 @@
-import React, {useEffect, useState} from 'react'
-import styled from 'styled-components';
-import axios from 'axios'
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import axios from "axios";
 
 const SearchBar = () => {
-
-    const [character, setCharacter] = useState();
-    const [startsWith, setStartsWith] = useState('');
+  const [character, setCharacter] = useState();
+  const [startsWith, setStartsWith] = useState("");
   useEffect(() => {
-       axios.get(`http://gateway.marvel.com/v1/public/characters?nameStartsWith=${startsWith}&apikey=${process.env.REACT_APP_API_KEY}&hash=${process.env.REACT_APP_HASH_KEY}&limit=10`)
-       .then(res => {
-        setCharacter(res.data.data.results)
-       })
-       .catch(err => {
+    axios
+      .get(
+        `http://gateway.marvel.com/v1/public/characters?nameStartsWith=${startsWith}&apikey=${process.env.REACT_APP_API_KEY}&hash=${process.env.REACT_APP_HASH_KEY}&limit=5`
+      )
+      .then((res) => {
+        setCharacter(res.data.data.results);
+      })
+      .catch((err) => {
         console.log(err);
-       })
-  },[startsWith])
+      });
+  }, [startsWith]);
 
   const handleNameChange = (e) => {
     e.preventDefault();
-    setStartsWith(e.target.value.trim())
-    console.log(startsWith);
-  }
+    const input = e.target.value.trim();
+    setStartsWith(input);
+  };
 
   return (
-    <>
-    <StyledSearch>
-    <input type='text' placeholder='Search Character...' onChange={handleNameChange}/>
-    </StyledSearch>
+    <StyledForm>
+        <input
+          type="text"
+          placeholder="Search Character..."
+          onChange={handleNameChange}
+        />
+        {startsWith.length === 0 || character?.length === 0 ? (
+          <></>
+        ) : (
+          <StyledCharacters>
+            {character &&
+              character?.map((c) => {
+                return <p>{c.name}</p>;
+              })}
+          </StyledCharacters>
+        )}
+    </StyledForm>
+  );
+};
 
-    {
-      startsWith.length === 0 || character?.length === 0 ? (
-        <StyledErrorHandle>No Characters Found</StyledErrorHandle>
-      ) :     
-        character && character?.map(c => {
-          return (
-            <StyledResults>
-            <p>{c.name}</p>
-            </StyledResults>
-          )
-        })
-    }
-    </>
-  )
-}
+export default SearchBar;
 
-export default SearchBar
-
-const StyledSearch = styled.div`
-  display: flex;
-  justify-content: center;
-  input {
-    padding: 5px;
+const StyledForm = styled.div`
+text-align: center;
+width: 200px;
+margin: 20px auto;
+input {
+  padding: 10px;
+  width: 100%;
+  :focus{
+    outline: none;
   }
-`
-
-const StyledErrorHandle = styled.div`
-  display: flex;
-  justify-content: center;
-  margin-top: 5px;
-`
-
-const StyledResults = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  flex-direction: column;
-  background-color: white;
-  box-shadow: rgba(0,0,0,0.35) 0px 5px 15px;
-  height: 200px;
-  width: 50vw;
-  margin-top: 10px;
-  margin: auto;
-  overflow: hidden;
-  overflow-y: auto;
-p:hover {
-  background-color: lightgrey;
-  cursor: pointer;
 }
+p{
+  padding: 5px;
+  font-size: 15px;
+  font-weight: bold;
+  text-align: left;
+  :hover{
+    background-color: grey;
+  }
+}
+`
+
+const StyledCharacters = styled.div`
+  border: 1px solid grey;
 `
